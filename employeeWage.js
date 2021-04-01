d2d392aa8f6138f88ecd8403b90e1dbd22b6ab96
viresh rawool <vireshrawool007@gmail.com>
2021-04-01 08:47:46 +0530

    refactor by using function

--- employeeWage.js
+++ employeeWage.js
@@ -4,18 +4,19 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHours(empCheck) {
+  switch (empCheck) {
+    case IS_PART_TIME:
+      return PART_TIME_HOURS;
+    case IS_Full_TIME:
+      return FULL_TIME_HOURS;
+    default:
+      return 0;
+  }
+}
+
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_Full_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-        default:
-            empHrs=0
-}
-let empWage = empHrs* WAGE_PER_HOUR;
-  console.log("Employee Wage ",empWage);
-
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage ", empWage);
