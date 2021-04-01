1be621b50a13cd0fecc7344ed98f76b5c0b26c87
viresh rawool <vireshrawool007@gmail.com>
2021-04-01 08:52:34 +0530

    refactor using for loop

--- employeeWage.js
+++ employeeWage.js
@@ -3,6 +3,7 @@ const IS_Full_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 4;
 
 function getWorkingHours(empCheck) {
   switch (empCheck) {
@@ -16,7 +17,9 @@ function getWorkingHours(empCheck) {
 }
 
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage ", empWage);
