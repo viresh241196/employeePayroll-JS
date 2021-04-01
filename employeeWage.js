8bcb41e0a7cb8a2a2735477774efb80d1ca68a29
viresh rawool <vireshrawool007@gmail.com>
2021-04-01 09:01:22 +0530

    refactor by using while loop to calculate for month

--- employeeWage.js
+++ employeeWage.js
@@ -3,7 +3,8 @@ const IS_Full_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 4;
+const NUM_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 100;
 
 function getWorkingHours(empCheck) {
   switch (empCheck) {
@@ -17,9 +18,11 @@ function getWorkingHours(empCheck) {
 }
 
 let empHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+let workingDays = 0;
+while (empHrs <= MAX_HRS_IN_MONTH && workingDays < NUM_OF_WORKING_DAYS) {
+  workingDays++;
   let empCheck = Math.floor(Math.random() * 10) % 3;
   empHrs += getWorkingHours(empCheck);
 }
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage ", empWage);
+console.log("Employee Wage ", empWage+" total days ",workingDays+" total hours ",empHrs);
