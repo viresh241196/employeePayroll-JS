ceeb05f09759ea23733e0e7a87f5be8f9613425d
viresh241196 <vireshrawool007@gmail.com>
2021-04-02 15:44:05 +0530

    added parameter to store gender and date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,12 +2,15 @@ class EmployeePayrollData {
   id;
   salary;
   name;
+  gender;
+  startDate;
 
   //constructor
-  constructor(id, name, salary) {
+  constructor(id, name, salary, gender, startDate) {
     this.id = id;
     this.name = name;
     this.salary = salary;
+    (this.gender = gender), (this.startDate = startDate);
   }
   //getter and setter method
   get name() {
@@ -19,7 +22,23 @@ class EmployeePayrollData {
 
   //method
   toString() {
-    return "id=" + this.id + ",name= " + this.name + ",salary=" + this.salary;
+    const options = { year: "numeric", moth: "long", day: "numeric" };
+    const empDate =
+      this.startDate === undefined
+        ? "undefined"
+        : this.startDate.toLocaleDateString("en-US", options);
+    return (
+      "id=" +
+      this.id +
+      ",name= " +
+      this.name +
+      ",salary=" +
+      this.salary +
+      ",gender=" +
+      this.gender +
+      ",startDate =" +
+      empDate
+    );
   }
 }
 
@@ -27,3 +46,11 @@ let employeePayrollData = new EmployeePayrollData(1, "viresh", 30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "sidhu";
 console.log(employeePayrollData.toString());
+let newemployeePayrollData = new EmployeePayrollData(
+  2,
+  "terrisa",
+  3000,
+  "f",
+  new Date()
+);
+console.log(newemployeePayrollData.toString());
